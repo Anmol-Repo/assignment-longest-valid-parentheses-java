fb4df7be4bf23668ba757d970c3d92718e4dadf0
Anmol-Repo <148074536+Anmol-Repo@users.noreply.github.com>
2026-07-21 13:26:22 +0530

    Update Solution.java
    Updated comments for clarity and consistency.

--- Solution.java
+++ Solution.java
@@ -8,45 +8,30 @@ public class Solution {
         Stack<Integer> stack = new Stack<>();
         int max = 0;
 
-        // Initial boundary
+        // Starting boundary
         stack.push(-1);
          for (int i = 0; i < s.length(); i++) {
-
             if (s.charAt(i) == '(') {
-
                 stack.push(i);
-
             } else {
-
                 stack.pop();
-
                 if (stack.isEmpty()) {
-
-                    stack.push(i);
-
+                  stack.push(i);
                 }  else {
-
                     max = Math.max(max, i - stack.peek());
-
                 }
           }
         }
-
         return max;
     }
-
-
-  
-    public static void main(String[] args) {
+ public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter parentheses string: ");
         String s = sc.nextLine();
 
         int ans = longestValidParentheses(s);
-
-        System.out.println("Longest Valid Parentheses Length = " + ans);
+        System.out.println("Longest Valid Parentheses = " + ans);
 
         sc.close();
     }
