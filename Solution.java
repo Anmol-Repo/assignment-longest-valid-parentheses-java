a39f22a80588e2a46acb4256875229bd60d329d3
Anmol-Repo <148074536+Anmol-Repo@users.noreply.github.com>
2026-07-21 13:18:21 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+
+    public static int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        // Sentinel value
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+
+                stack.push(i);
+
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+
+                    stack.push(i);
+
+                }  else {
+
+                    max = Math.max(max, i - stack.peek());
+
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter parentheses string: ");
+        String s = sc.nextLine();
+
+        int ans = longestValidParentheses(s);
+
+        System.out.println("Longest Valid Parentheses Length = " + ans);
+
+        sc.close();
+    }
+}
